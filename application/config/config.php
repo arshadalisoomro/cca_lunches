df10e88e9cccd1701620b3c41e6cdf039d563d68
Eric Totten <erictotten@cox.net>
2014-05-21 10:38:08 -0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -93,7 +93,7 @@ define('AVATAR_DEFAULT_IMAGE', 'default.jpg');
 define('COOKIE_RUNTIME', 1209600);
 // the domain where the cookie is valid for, for local development ".127.0.0.1" and ".localhost" will work
 // IMPORTANT: always put a dot in front of the domain, like ".mydomain.com" !
-define('COOKIE_DOMAIN', '.erictotten.info');
+define('COOKIE_DOMAIN', '.ccalunches.com');
 
 /**
  * Configuration for: Database
